0346934597ee73542042c000b72313ac374a5cea
Andrew Shpagin <andrewshpagin@gmail.com>
2023-01-28 00:55:26 +0200

    errors

--- index.js
+++ index.js
@@ -71,6 +71,13 @@ const socketio = new Server(server,{
 
 app.use(cors(corsOptions));
 
+socketio.engine.on("connection_error", (err) => {
+    console.log(err.req);      // the request object
+    console.log(err.code);     // the error code, for example 1
+    console.log(err.message);  // the error message, for example "Session ID unknown"
+    console.log(err.context);  // some additional error context
+  });
+
 socketio.on('connection', socket => {
     console.log('User connected');
 
